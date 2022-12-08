e0d2321c6fb7be9920374ae1ed85b2123aa089e7
anazyazd <anazyazd@gmail.com>
2022-12-08 13:54:56 -0600

    Signup page basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { GlobalStyle } from './styles/GlobalStyle'
-
-import { Greetings } from './components/Greetings'
+// import { GlobalStyle } from './styles/GlobalStyle'
+// import { Greetings } from './components/Greetings'
+import { HashRouter, Route } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
 
 export function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Greetings />
-    </>
+    <HashRouter>
+      <div>
+        <Route path="/" component= { Login }/>
+        <Route path="/Signup" component= { Signup}/>
+      </div>
+    </HashRouter>
   )
 }
