7f582178bf77e70209fb9bc0930fd5c3e121810b
anazyazd <anazyazd@gmail.com>
2022-12-12 16:26:30 -0600

    react-routers and types installed for hash routers, login.tsx draft, signup.tsx draft, and app.tsx with new hashrouter routes added > > Co-authored-by: Andrea Azadeh Yazdandoost anazyazd@gmail.com Co-authored-by: Andy Wong andy.wong.km27@gmail.com Co-authored-by: Bo Kim bokim.ce@gmail.com Co-authored-by: Chihiro Shibayama Snider chihiro.shibayama@gmail.com Co-authored-by: Coral Dabarera Edelson corald@gmail.com

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // import { GlobalStyle } from './styles/GlobalStyle'
 // import { Greetings } from './components/Greetings'
+import * as React from 'react';
+import * as ReactDOM from "react-dom"
 import { HashRouter, Route } from "react-router-dom";
-import { Login } from "./components/Login";
-import { Signup } from "./components/Signup";
+import Login  from "./components/Login";
+import SignUp from "./components/Signup";
 
 export function App() {
   return (
     <HashRouter>
       <div>
-        <Route path="/" component= { Login }/>
-        <Route path="/Signup" component= { Signup}/>
+        <Route path="/Login" element= { <Login />} />
+        <Route path="/Signup" element= { <SignUp />} />
       </div>
     </HashRouter>
   )
