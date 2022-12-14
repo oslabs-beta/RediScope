78d3b9e3a7df8c358ea78a5d87eb13371df09d12
AndyW827 <andy.wong.km27@gmail.com>
2022-12-14 11:25:28 -0800

    redis controller sends json data back to client

--- server/server.js
+++ server/server.js
@@ -38,6 +38,17 @@ app.get('/api/flasks', async (req, res) => {
 
 app.use('/api/redis', redisRouter)
 
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  }
+  const errorObj = Object.assign({}, defaultErr, err)
+  console.log(errorObj.log)
+  return res.status(errorObj.status).json(errorObj.message)
+})
+
 // let { ipcMain } = require("electron")
 
 // ipcMain.handle("console", (event, line) => {
