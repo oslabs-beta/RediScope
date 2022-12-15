53bc667d268be51db90478c59cc02b577b9c4490
AndyW827 <andy.wong.km27@gmail.com>
2022-12-14 19:46:18 -0800

    feat: fetched and render chart in dashboard Co-authored-by: Andrea Azadeh Yazdandoost <anazyazd@gmail.com> Co-authored-by: Andy Wong   <andy.wong.km27@gmail.com> Co-authored-by: Bo Kim <bokim.ce@gmail.com> Co-authored-by: Chihiro Shibayama Snider <chihiro.shibayama@gmail.com> Co-authored-by: Coral Dabarera Edelson <corald@gmail.com>

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,126 @@
-import * as React from 'react'
-import PieChart from './Charts/pieChart'
+import React, { useState } from 'react'
+import PieChart from './Charts/PieChart'
+import BarChart from './Charts/BarChart'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
-function Dashboard() {
+type Props = {}
+
+type URLState = {
+  URL: string | void
+}
+
+export default function Dashboard(
+  props: Props,
+  { URL }: URLState
+): JSX.Element {
+  // const state = {
+  //   URL: ''
+  // }
+  const [inputField, setInputField] = useState('')
+
+  // const handleSubmit = async (formValue?: string): Promise<any> => {
+  //   // const URL = formValue
+  //   // console.log(URL)
+  //   // const fetchData = async () => {
+  //   try {
+  //     const res = await fetch('http://localhost:4000/api/redis', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       mode: 'no-cors',
+  //       body: JSON.stringify(
+  //         'redis://default:rediscope123@redis-15161.c53.west-us.azure.cloud.redislabs.com:15161'
+  //       ),
+  //     })
+  //     const data = await res.json()
+  //     console.log(data)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  const handleSubmitMVP = async (): Promise<any> => {
+    // const URL = formValue
+    // console.log(URL)
+    // const fetchData = async () => {
+    try {
+      console.log('handleclick MVP')
+      const res = await fetch('http://localhost:4000/api/redis', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        mode: 'no-cors',
+        body: JSON.stringify({
+          url: 'redis://default:rediscope123@redis-15161.c53.west-us.azure.cloud.redislabs.com:15161',
+        }),
+      })
+      console.log(res)
+      if (res.ok !== false) {
+        const data = await res.json()
+        console.log(data)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // setInputField()
+
+  const validationSchema = () => {
+    return Yup.object().shape({
+      URL: Yup.string().required(
+        'Please insert URL to connect to your Redis Cache Database'
+      ),
+    })
+  }
+
+  const initVal = {
+    URL: '',
+  }
+
   return (
     <>
       <div>
-        <h1>This is our Dashboard page</h1>
+        <div>
+          <h2> Redis URI: </h2>
+          {/* <input>Redis URI: </input> */}
+          <button
+            type="submit"
+            className="submit-btn"
+            onClick={handleSubmitMVP}
+          >
+            Submit123
+          </button>
+          {/* <Formik
+            initialValues={initVal}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          > */}
+          {/* <Form>
+              <div className="URL-Form">
+                <label htmlFor="Connect-URL">Redis Cache URL</label>
+                <Field
+                  name="Connect-URL"
+                  type="text"
+                  className="URL-form-control"
+                />
+              </div> */}
+          {/* <div> */}
+
+          {/* <button type="submit" className="btn">
+                  <span>Submit</span>
+                </button>
+              </div>
+            </Form> */}
+          {/* </Formik> */}
+        </div>
+        <h1>Dashboard</h1>
+        <BarChart />
         <PieChart />
       </div>
     </>
   )
 }
 
-export default Dashboard
+// export default Dashboard
