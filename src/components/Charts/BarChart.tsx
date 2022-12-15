b15359e4e76adc9135cb19f6a3872fd3710add21
AndyW827 <andy.wong.km27@gmail.com>
2022-12-14 19:46:18 -0800

    feat: fetched and render chart in dashboard Co-authored-by: Andrea Azadeh Yazdandoost <anazyazd@gmail.com> Co-authored-by: Andy Wong   <andy.wong.km27@gmail.com> Co-authored-by: Bo Kim <bokim.ce@gmail.com> Co-authored-by: Chihiro Shibayama Snider <chihiro.shibayama@gmail.com> Co-authored-by: Coral Dabarera Edelson <corald@gmail.com>

---
+++ src/components/Charts/BarChart.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+// Fetch to api/redis to grab used memory & available memory metrics
+// const fetchData = async () => {
+//   try {
+//     await fetch('http://localhost:4000/api/redis', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(
+//         'redis://default:rediscope123@redis-15161.c53.west-us.azure.cloud.redislabs.com:15161'
+//       ),
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+//fetchData()
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Memory',
+    },
+  },
+}
+
+const labels = ['Current Memory in Megabytes']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Used Memory MB',
+      data: [1.2],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Total Available Memory MB',
+      data: [1.9],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+}
+
+export default function BarChart() {
+  return <Bar options={options} data={data} />
+}
