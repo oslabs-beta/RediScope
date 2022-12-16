2c60b88f691eca624bdc836cf18169d657286a12
anazyazd <anazyazd@gmail.com>
2022-12-16 12:34:51 -0600

    hard-coded data and styling for presentations > > Co-authored-by: Andrea Azadeh Yazdandoost anazyazd@gmail.com Co-authored-by: Andy Wong andy.wong.km27@gmail.com Co-authored-by: Bo Kim bokim.ce@gmail.com Co-authored-by: Chihiro Shibayama Snider chihiro.shibayama@gmail.com Co-authored-by: Coral Dabarera Edelson corald@gmail.com

--- src/components/Charts/pieChart.tsx
+++ src/components/Charts/pieChart.tsx
@@ -4,27 +4,33 @@ import { Pie } from 'react-chartjs-2'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Cache Hit Ratio',
+    },
+  },
+}
+
+
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['keyspace_hits', 'keyspace_misses'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
       ],
       borderColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
       ],
       borderWidth: 1,
     },
@@ -32,5 +38,11 @@ export const data = {
 }
 
 export default function PieChart() {
-  return <Pie data={data} />
+  return (
+    <>
+      <h6>cache hit</h6>
+    <Pie options={options} data={data} />
+  </>
+  )
+  
 }
