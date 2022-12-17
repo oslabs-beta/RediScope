101e72ae583034f59a3d44447baab3e32d11e21c
anazyazd <anazyazd@gmail.com>
2022-12-17 13:19:24 -0600

    type descriptive commit message here don't close the quotation here > > Co-authored-by: Andrea Azadeh Yazdandoost anazyazd@gmail.com Co-authored-by: Andy Wong andy.wong.km27@gmail.com Co-authored-by: Bo Kim bokim.ce@gmail.com Co-authored-by: Chihiro Shibayama Snider chihiro.shibayama@gmail.com Co-authored-by: Coral Dabarera Edelson corald@gmail.com

---
+++ src/service/authentication.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const API_URL = '';
+
+class AuthService {
+    login(username: string, password: string){
+        return axios
+          .post(API_URL + "sign-in", {
+            username, password
+          })
+          .then(res => {
+            if (res.data){
+                localStorage.setItem('user', JSON.stringify(res.data));
+            }
+            return res.data;
+          });
+    }
+
+    signup(username: string, password: string, email: string){
+        return axios.post(API_URL + 'signup', {
+            username,
+            password,
+            email
+        });
+    }
+}
+
+export default new AuthService();
