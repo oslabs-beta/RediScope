0e802792193bfe0373c1758ccf82985e9f8e356f
Coral Dabarera Edelson <corald@gmail.com>
2022-12-17 13:29:50 -0800

    Co-authored-by: Andrea Azadeh Yazdandoost anazyazd@gmail.com Co-authored-by: Andy Wong andy.wong.km27@gmail.com Co-authored-by: Bo Kim bokim.ce@gmail.com Co-authored-by: Chihiro Shibayama Snider chihiro.shibayama@gmail.com Co-authored-by: Coral Dabarera Edelson corald@gmail.com

--- server/db/db.tsx
+++ server/db/db.tsx
@@ -12,7 +12,7 @@ const pool = new Pool({ connectionString: process.env.PG_URI });
 // which is a function that returns the invocation of pool.query() after logging the query
 // This will be required in the controllers to be the access point to the database
 module.exports = {
-  query: (text, params, callback) => {
+  query: (text: string, params: any[], callback: Function) => {
     console.log('executed query', text);
     return pool.query(text, params, callback);
   },
