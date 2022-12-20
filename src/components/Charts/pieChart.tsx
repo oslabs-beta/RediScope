c38fd3a97d4d34f74287c9d49cec4f4f1bd6a481
AndyW827 <andy.wong.km27@gmail.com>
2022-12-19 19:14:01 -0800

    refactor: refactored server.js and redisconnect middleware / feat: passed in data to graphs / chore: added typescript to redisform and graphs components
    >
    >
    Co-authored-by: Andrea Azadeh Yazdandoost <anazyazd@gmail.com>
    Co-authored-by: Andy Wong
      <andy.wong.km27@gmail.com>
    Co-authored-by: Bo Kim <bokim.ce@gmail.com>
    Co-authored-by: Chihiro Shibayama Snider <chihiro.shibayama@gmail.com>
    Co-authored-by: Coral Dabarera Edelson <corald@gmail.com>

--- src/components/Charts/pieChart.tsx
+++ src/components/Charts/pieChart.tsx
@@ -1,48 +1,50 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'react-chartjs-2'
+import { RedisContext } from '../../context/RedisContext'
+import { optionsType, dataType } from '../../@types/chartjsTypes'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Cache Hit Ratio',
-    },
-  },
-}
+export default function PieChart(): JSX.Element {
+  const { redisData, setRedisData } = useContext(RedisContext)
+  console.log('redisData in pieChart', redisData)
 
+  // assigning data to table.  default to 0 if there is no data or undefined.
+  let keyspaceHits: number = redisData?.keyspace_hits || 0
+  let keyspaceMisses: number = redisData?.keyspace_misses || 0
+  console.log('keyspaceHits, keyspaceMisses', keyspaceHits, keyspaceMisses)
 
-export const data = {
-  labels: ['keyspace_hits', 'keyspace_misses'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-      ],
-      borderWidth: 1,
+  const options: optionsType = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Cache Hit Ratio',
+      },
     },
-  ],
-}
+  }
 
-export default function PieChart() {
+  const data: dataType = {
+    labels: ['keyspace_hits', 'keyspace_misses'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [keyspaceHits, keyspaceMisses],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  }
+
   return (
     <>
-      <h6>cache hit</h6>
-    <Pie options={options} data={data} />
-  </>
+      {/* <h6>keyspace_hits: {keyspaceHits} keyspace_misses: {keyspaceMisses}</h6> */}
+      <Pie options={options} data={data} />
+    </>
   )
-  
 }
