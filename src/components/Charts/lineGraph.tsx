e95201f634b7c8f6c3c5c370784280a8bc622137
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2022-12-22 20:28:56 -0800

    fetches redis metrics using setInterval, save the data to the local storage, set that to the state, then render it to the line graph component
    Co-authored-by: Andrea Azadeh Yazdandoost <anazyazd@gmail.com>
    Co-authored-by: Andy Wong
    <andy.wong.km27@gmail.com>
    Co-authored-by: <Bo Kim bokim.ce@gmail.com>
    Co-authored-by: Chihiro Snider <chihirosnider@gmail.com>
    Co-authored-by: Coral Dabarera Edelson <corald@gmail.com>

--- src/components/Charts/lineGraph.tsx
+++ src/components/Charts/lineGraph.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,6 +10,8 @@ import {
   Legend,
 } from 'chart.js'
 import { Line } from 'react-chartjs-2'
+import { RedisContext } from '../../context/RedisContext'
+
 // import { faker } from '@types/faker'
 
 ChartJS.register(
@@ -30,49 +32,32 @@ export const options: object = {
     },
     title: {
       display: true,
-      text: 'Memory Usage',
+      text: 'Memory Usage Over Time (today)',
     },
   },
 }
 
-const labels: string[] = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-]
-// I need to figure out how to put data types to these data: I used to have data: object, and when I did, it gave me errors inside of LineGraph components return statement below
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: [1, 5, 2, 4, 6, 2, 7, 0],
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
-}
-
-export const data2 = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 2',
-      data: [7, 4, 3, 10, 2, 1, 5, 8],
-      borderColor: '#32C1BA',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
-}
-
 export function LineGraph() {
+  const { localMemories, setLocalMemories } = useContext(RedisContext)
+  const { localMemoriesTime, setLocalMemoryTime } = useContext(RedisContext)
+  console.log('localmemory state: ', localMemories)
+  console.log('localmemoryTime state: ', localMemoriesTime)
+
+  const labels: number[] = localMemoriesTime
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: localMemories,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  }
   return <Line options={options} data={data} />
 }
 
-export function LineGraph2() {
-  return <Line options={options} data={data2} />
-}
+// export function LineGraph2() {
+//   return <Line options={options} data={data2} />
+// }
--- src/components/RedisForm.tsx
+++ src/components/RedisForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import axios from 'axios'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
@@ -11,24 +11,68 @@ type URLState = {
 
 function RedisForm(props: Props): JSX.Element {
   const { redisData, setRedisData } = useContext(RedisContext)
+  const { localMemories, setLocalMemories } = useContext(RedisContext)
+  const { localMemoriesTime, setLocalMemoryTime } = useContext(RedisContext)
 
   const submitHandler = async (formValue: object): Promise<any> => {
     try {
       const res = await axios.post('http://localhost:4000/api/redis', formValue)
+      let timesRun = 0
+      console.log(timesRun)
+      const getData = async () => {
+        if (timesRun < 3) {
+          const response = await axios.post(
+            'http://localhost:4000/api/redis',
+            formValue
+          )
+          console.log(response.data.used_memory)
+          const timeStamp = new Date()
+          sessionStorage.setItem(
+            `${timeStamp.toLocaleString()}`,
+            `${response.data.used_memory}`
+          )
+          timesRun += 1
+        } else {
+          clearInterval(getMemory)
+          const memoryDataAll = Object.entries(sessionStorage)
+          const timeData = memoryDataAll.map(data => data[0])
+          const memoryDataOnly = memoryDataAll.map(data => parseInt(data[1]))
+          setLocalMemories(memoryDataOnly)
+          setLocalMemoryTime(timeData)
+          console.log('localmemory state: ', localMemories)
+          console.log('localmemoryTime state: ', localMemoriesTime)
+          // console.log('memory data from Object.entries', memoryDataAll)
+          // console.log('memory data array: ', memoryDataOnly)
+          // console.log('time stamp array: ', timeData)
+        }
+      }
+      const getMemory = setInterval(getData, 7000)
       // IF FETCH IS SUCCESSFUL, SET DATA TO CONTEXT
       if (res.status === 200) {
         setRedisData(res.data)
         console.log('res data', res)
         console.log('redisData', redisData)
-      } else {
       }
     } catch (error) {
       console.log(error)
     }
   }
 
+  const handleStart = async (formValue: object): Promise<any> => {
+    try {
+      // IF FETCH IS SUCCESSFUL, SET DATA TO CONTEXT
+      // if (res.status === 200) {
+      //   setRedisData(res.data)
+      //   console.log('res data', res)
+      //   console.log('redisData', redisData)
+      // }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const initVal: URLState = {
-    URL: '',
+    URL: 'redis://default:olYVwjlh1PxPEb3Pia3od0QbpE7DAhIu@redis-13288.c83.us-east-1-2.ec2.cloud.redislabs.com:13288',
   }
 
   const validationSchema = () => {
@@ -62,4 +106,8 @@ function RedisForm(props: Props): JSX.Element {
 
 export default RedisForm
 
+// Andy Redis Account
 // redis://default:rediscope123@redis-15161.c53.west-us.azure.cloud.redislabs.com:15161
+
+// Chihiro Redis Account
+// redis://default:olYVwjlh1PxPEb3Pia3od0QbpE7DAhIu@redis-13288.c83.us-east-1-2.ec2.cloud.redislabs.com:13288
