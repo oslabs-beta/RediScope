2ce6b6df9f497998dac303cb64e7d7711737d671
Coral Dabarera Edelson <corald@gmail.com>
2023-01-02 20:10:50 -0800

    all graphs working, need to check on cachehitratio Co-authored-by: Andrea Azadeh Yazdandoost <anazyazd@gmail.com> Co-authored-by: Andy Wong <andy.wong.km27@gmail.com> Co-authored-by: Bo Kim <bokim.ce@gmail.com> Co-authored-by: Chihiro Snider <chihirosnider@gmail.com> Co-authored-by: Coral Dabarera Edelson <corald@gmail.com>

--- src/components/Charts/graphCacheHitRatio.tsx
+++ src/components/Charts/graphCacheHitRatio.tsx
@@ -22,12 +22,15 @@ ChartJS.register(
   Legend
 )
 
-// collect and render total evicted keys 
-export function EvictedKeys() {
-  const { evictedKeys, setEvictedKeys } = useContext(RedisContext)
+// I need to figure out how to put data types to these data: I used to have data: object, and when I did, it gave me errors inside of LineGraph components return statement below
+
+export function GraphCacheHitRatio() {
   const { time, setTime } = useContext(RedisContext)
-  console.log('from evicted keys', evictedKeys)
- 
+  const { keyHits, setKeyHits } = useContext(RedisContext)
+  const { keyMisses, setKeyMisses } = useContext(RedisContext)
+  const { rss, setRss } = useContext(RedisContext);
+
+  // console.log('from linegraph', usedMemory[usedMemory.length])
 
   const options: object = {
     responsive: true,
@@ -37,7 +40,7 @@ export function EvictedKeys() {
       },
       title: {
         display: true,
-        text: 'Evicted Keys',
+        text: 'Cache Hit Ratio',
       },
     },
   }
@@ -47,11 +50,17 @@ export function EvictedKeys() {
     labels,
     datasets: [
       {
-        label: 'current Evicted Keys',
-        data: evictedKeys,
+        label: 'Key Space Hits',
+        data: keyHits,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
+      {
+        label: 'Key Space Misses',
+        data: keyMisses,
+        borderColor: 'rgb(79, 189, 186)',
+        backgroundColor: 'rgba(79, 189, 186, 0.5)',
+      }
     ],
   }
   return <Line options={options} data={data} />
